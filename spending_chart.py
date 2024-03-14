dc51b2649db3d5192fd82f636590c8555dc4e7b2
Alex <carl6329@umn.edu>
2024-03-14 17:00:45 -0500

    adjusted chart colors

--- spending_chart.py
+++ spending_chart.py
@@ -1,10 +1,15 @@
 import PyQt6.QtCharts as QCharts
 import PyQt6.QtGui as QGUI
+import random
 
 class SpendingChart(QCharts.QChart):
     def __init__(self, data):
         super().__init__()
         self.data = data
+        self.colors = ["#36175E", '#005f73', '#0a9396', '#94d2bd',
+                '#e9d8a6', '#ee9b00',  '#ca6702',
+                '#bb3e03', '#ae2012', '#9b2226']
+
 
         self.outer = QCharts.QPieSeries()
         self.set_outer()
@@ -12,9 +17,11 @@ class SpendingChart(QCharts.QChart):
     
     def set_outer(self):
         slices = []
+        color = 0
         for d in self.data[:-1]:
             sliver = QCharts.QPieSlice(d[0], d[1], parent = None)
-            sliver.setColor(QGUI.QColor("#82d3e5"))
+            sliver.setColor(QGUI.QColor(self.colors[color]))
+            color += 1
 
             slices.append(sliver)
             self.outer.append(sliver)
@@ -26,4 +33,3 @@ class SpendingChart(QCharts.QChart):
         self.outer = QCharts.QPieSeries()
         self.set_outer()
         self.addSeries(self.outer)
-
